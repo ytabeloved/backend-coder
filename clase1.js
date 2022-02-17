230f361168060e03e2d3d6755cb0e437052913ac
ytabeloved <karlisvaldi@gmail.com>
2022-02-17 17:53:10 -0300

    clase echa

---
+++ clase1.js
@@ -0,0 +1,8 @@
+class User {
+    constructor(nombre, apellido, libros, mascotas){
+        this.nombre= nombre;
+        this.apellido=apellido;
+        this.libros=libros;
+        this.mascotas=mascotas;
+    }
+}
