dd68af2f5b959fd7efd8958437c45fa8ecf4f997
ytabeloved <karlisvaldi@gmail.com>
2022-02-18 11:54:17 -0300

    primer method funcionando

--- clase1.js
+++ clase1.js
@@ -5,4 +5,35 @@ class User {
         this.libros=libros;
         this.mascotas=mascotas;
     }
+
+    //methods
+
+    getFullName(){
+       return `nombre completo es ${this.nombre} ${this.apellido}`;
+    }
+
+    addMascota(String){
+        this.mascotas =[];
+        this.mascotas.push(String)       
+
+    }
+
+    countMascotas(){
+
+    }
+
+    addBook(String, String){
+
+    }
+
+    getBookNames(){
+
+    }
 }
+
+
+const p = new User(`karla`, 'vergara', `edgar allan poe`, `perro`)
+
+console.log(p.getFullName())
+console.log(p.addMascota(`gato`))
+console.log(p.mascotas);
