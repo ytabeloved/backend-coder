a22d0d0a93c57df2049fd1ad9ebf252e2e7ecc67
ytabeloved <karlisvaldi@gmail.com>
2022-02-22 22:48:04 -0300

    ejercicio 1

---
+++ .vscode/main.js
@@ -0,0 +1,23 @@
+let Num=[]
+let countObj={}
+
+const randNum = () =>{
+    for(i=0; i<10;i++){
+        Num.push(Math.floor(Math.random()*21)+1)
+    }
+    console.log(Num)
+}
+
+
+let countFunc = keys => {
+    countObj[keys] = ++countObj[keys] || 1;
+}
+
+
+
+randNum();
+Num.forEach(countFunc)
+console.log(Num)
+console.log(countObj);
+
+
